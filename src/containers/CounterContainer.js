1b9efdfdc467b78fd669ec6c5d39e50bfc189a6b
Dongyeong Kim <userset301@gmail.com>
2024-10-07 23:44:56 +0900

    counter miss

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,22 +1,17 @@
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-export default connect(
-  (state) => ({ number: state.counter.number }),
-  (dispatch) =>
-    bindActionCreators(
-      {
-        increase,
-        decrease,
-      },
-      dispatch,
-    ),
-)(CounterContainer);
+export default CounterContainer;
